58a885c087a1a397e2c6ff32a85b7919a1bad2d7
Aleksey Shiyanov <ashiyanov@baccasoft.ru>
2019-03-01 18:28:23 +0400

    Add swagger

--- src/main/java/ru/bacca/bikerun/config/AppConfig.java
+++
@@ -1,7 +0,0 @@
-package ru.bacca.bikerun.config;
-
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
-
-
-public class AppConfig extends GlobalMethodSecurityConfiguration {
-}
