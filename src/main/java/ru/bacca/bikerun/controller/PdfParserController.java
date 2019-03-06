60088a5fc1a307f68c2be1807eb5e5d05e330c5a
Aleksey Shiyanov <ashiyanov@baccasoft.ru>
2019-03-06 18:47:38 +0400

    Done with pdf parser

--- src/main/java/ru/bacca/bikerun/controller/PdfParserController.java
+++ src/main/java/ru/bacca/bikerun/controller/PdfParserController.java
@@ -5,17 +5,13 @@ import com.documents4j.api.IConverter;
 import com.documents4j.job.LocalConverter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 @RestController
 @RequestMapping("/file")
@@ -47,5 +43,19 @@ public class PdfParserController {
 
         LOGGER.info("Converting pdf to word successful");
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/read")
+    public String readTextFromWord(){
+        try (FileInputStream fileInputStream = new FileInputStream(DOCX_PATH)) {
+            XWPFDocument docxFile = new XWPFDocument(fileInputStream);
+
+//            LOGGER.info(docxFile.getTables().get(0).getRow(1).getCell(1).getText());
+            docxFile.getParagraphs().forEach(xwpfParagraph -> LOGGER.info(xwpfParagraph.getText()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "BAD";
+        }
+        return "OK";
     }
 }
