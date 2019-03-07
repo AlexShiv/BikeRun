1a01e4ee0ec09e5d571d69e60a1aee73c2ef1782
Aleksey Shiyanov <ashiyanov@baccasoft.ru>
2019-03-07 13:08:45 +0400

    wtf

--- src/main/java/ru/bacca/bikerun/controller/PdfParserController.java
+++ src/main/java/ru/bacca/bikerun/controller/PdfParserController.java
@@ -6,56 +6,71 @@ import com.documents4j.job.LocalConverter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import java.io.*;
+import java.util.Optional;
+import java.util.concurrent.Executor;
 
 @RestController
 @RequestMapping("/file")
 public class PdfParserController {
 
-    private static final String PDF_PATH = "D:\\response.pdf";
-    private static final String DOCX_PATH = "D:\\test.docx";
-
-
     private static final Logger LOGGER = LogManager.getLogger(PdfParserController.class);
+    private final Executor executor;
 
-    @PostMapping("/upload")
-    public ResponseEntity<HttpStatus> convertPdfToWord(@RequestParam("file") MultipartFile file){
-        LOGGER.info("Start converting pdf to word");
-
-        File docxFile = new File(DOCX_PATH);
-        File pdfFile = new File(PDF_PATH);
-        try (FileOutputStream fileOutputStream = new FileOutputStream(pdfFile)) {
-            fileOutputStream.write(file.getBytes());
-
-            IConverter converter = LocalConverter.make();
-            converter.convert(pdfFile).as(DocumentType.PDF)
-                    .to(docxFile).as(DocumentType.DOCX)
-                    .execute();
-        } catch (IOException e) {
-            LOGGER.error("Convertation failed - %m", e.getMessage());
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        LOGGER.info("Converting pdf to word successful");
-        return new ResponseEntity<>(HttpStatus.OK);
+    @Autowired
+    public PdfParserController(@Qualifier("threadPoolTaskExecutor") Executor executor) {
+        this.executor = executor;
     }
 
-    @GetMapping("/read")
-    public String readTextFromWord(){
-        try (FileInputStream fileInputStream = new FileInputStream(DOCX_PATH)) {
-            XWPFDocument docxFile = new XWPFDocument(fileInputStream);
+    @PostMapping("/upload")
+    public ResponseEntity<String> convertPdfToWord(ServletRequest servletRequest, ServletResponse servletResponse) throws IOException {
+        LOGGER.info("Start converting pdf to word");
 
-//            LOGGER.info(docxFile.getTables().get(0).getRow(1).getCell(1).getText());
-            docxFile.getParagraphs().forEach(xwpfParagraph -> LOGGER.info(xwpfParagraph.getText()));
+        IConverter converter = LocalConverter.make();
+        converter.convert(servletRequest.getInputStream()).as(DocumentType.PDF)
+                .to(servletResponse.getOutputStream()).as(DocumentType.DOCX)
+                .execute();
+        LOGGER.info("Converting pdf to word is over");
+
+        Optional<String> result = readTextFromWord(servletResponse.getOutputStream());
+
+        return result.map(s -> new ResponseEntity<>(s, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
+    }
+
+    private Optional<String> readTextFromWord(OutputStream outputStream) {
+        try (PipedInputStream in = new PipedInputStream()) {
+            try (PipedOutputStream out = new PipedOutputStream(in)) {
+                executor.execute(() -> {
+                    try {
+
+                    } catch (Exception e) {
+                        Thread t = Thread.currentThread();
+                        t.getUncaughtExceptionHandler().uncaughtException(t, e);
+                    }
+                });
+            }
+
+            XWPFDocument docxFile = new XWPFDocument(in);
+
+            String text = docxFile.getTables().get(0)
+                    .getRow(1)
+                    .getCell(1).getText();
+            return Optional.ofNullable(text);
         } catch (IOException e) {
             e.printStackTrace();
-            return "BAD";
+            return Optional.empty();
         }
-        return "OK";
     }
 }
